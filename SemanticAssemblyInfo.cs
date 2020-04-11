4105fcec77578f971f0ecde325345f434a127704
Bryan Apellanes <bryan@threeheadz.com>
2020-04-11 08:32:32 -0700

    updated semver file

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.3031174594")]
-[assembly: AssemblyCommit("f3ec2ed")]
-[assembly: AssemblySemanticVersion("1.0.5-test+f3ec2ed")]
+[assembly: AssemblyFileVersion("1.0.5.4141830454")]
+[assembly: AssemblyCommit("310419b")]
+[assembly: AssemblySemanticVersion("1.0.5-test+310419b")]

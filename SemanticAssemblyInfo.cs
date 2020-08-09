15e5eae31c02babf3e41d755e05e601c30fd3833
Bryan Apellanes <bryan@threeheadz.com>
2020-08-09 08:08:51 -0700

    updated build script and new dev version

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.26440224")]
-[assembly: AssemblyCommit("77705f2")]
-[assembly: AssemblySemanticVersion("1.0.5-test+77705f2")]
+[assembly: AssemblyFileVersion("1.0.5.3423221823")]
+[assembly: AssemblyCommit("c0acf7d")]
+[assembly: AssemblySemanticVersion("1.0.5-test+c0acf7d")]
